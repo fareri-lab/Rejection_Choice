1473ef8e94744e44aa03b97d86f21571a0d21ffd
Jordan Siegel Dejoie <86434055+jordansiegel@users.noreply.github.com>
2023-03-30 15:46:26 -0400

    JD
    Pushed updated participant list with 16 participants, pre and photo upload survey CSVs, archived old CSVs

--- updatedscoringscript_3.2.23.py
+++ updatedscoringscript_3.2.23.py
@@ -22,9 +22,9 @@ updatedchoicedata = pd.read_csv('shortformdata.csv')
 #%%
 
 #indexing data frame to isolate variables by condition
-neutralcondition = updatedchoicedata[updatedchoicedata['condition'] == 0]
-rejectioncondition = updatedchoicedata[updatedchoicedata['condition'] == 1]
-acceptcondition = updatedchoicedata[updatedchoicedata['condition'] == 2]
+neutralcondition = updatedchoicedata[updatedchoicedata['condition_recode'] == 0]
+rejectioncondition = updatedchoicedata[updatedchoicedata['condition_recode'] == 1]
+acceptcondition = updatedchoicedata[updatedchoicedata['condition_recode'] == 2]
 
 #%%
 
@@ -53,66 +53,67 @@ print(acceptchoice_std)
 
 #%%
 
-neutralsalience_mean = neutralcondition['salience'].mean()
-neutralsalience_std = neutralcondition['salience'].std()
+neutralsalience_mean = neutralcondition['salience_mean'].mean()
+neutralsalience_std = neutralcondition['salience_mean'].std()
 print('neutralsalience') 
 print(neutralsalience_mean)
 print(neutralsalience_std)
 
 #%%
 
-rejsalience_mean = rejectioncondition['salience'].mean()
-rejsalience_std = rejectioncondition['salience'].std()
+rejsalience_mean = rejectioncondition['salience_mean'].mean()
+rejsalience_std = rejectioncondition['salience_mean'].std()
 print('rejectionsalience')
 print(rejsalience_mean)
 print(rejsalience_std)
 
 #%%
 
-accsalience_mean = acceptcondition['salience'].mean()
-accsalience_std = acceptcondition['salience'].std()
+accsalience_mean = acceptcondition['salience_mean'].mean()
+accsalience_std = acceptcondition['salience_mean'].std()
 print('accsalience')
 print(accsalience_mean)
 print(accsalience_std)
 
 #%%
 
-neutralstress_mean = neutralcondition['stress'].mean()
-neutralstress_std = neutralcondition['stress'].std()
+neutralstress_mean = neutralcondition['recoded_stress'].mean()
+neutralstress_std = neutralcondition['recoded_stress'].std()
 print('neutralstress')
 print(neutralstress_mean)
 print(neutralstress_std)
 
 #%%
 
-rejstress_mean = rejectioncondition['stress'].mean()
-rejstress_std = rejectioncondition['stress'].std()
+rejstress_mean = rejectioncondition['recoded_stress'].mean()
+rejstress_std = rejectioncondition['recoded_stress'].std()
 print('rejstress')
 print(rejstress_mean)
 print(rejstress_std)
 
 #%%
 
-accstress_mean = acceptcondition['stress'].mean()
-accstress_std = acceptcondition['stress'].std()
+accstress_mean = acceptcondition['recoded_stress'].mean()
+accstress_std = acceptcondition['recoded_stress'].std()
 print('accstress')
 print(accstress_mean)
 print(accstress_std)
 
-#%%
-#paired samples t-test for condition by choice
-t_stat, p_value = ttest_rel(rejectioncondition['choice'],acceptcondition['choice'])
-print("T-statistic value:", t_stat)
-print('P-value:', p_value)
 
-#%% paired samples t-test for condition by salience
-t_stat, p_value = ttest_rel(rejectioncondition['salience'],acceptcondition['salience'])
-print("T-statistic value:", t_stat)
-print('P-value:', p_value)
+# #%%
+# #paired samples t-test for condition by choice
+# t_stat, p_value = ttest_rel(rejectioncondition['choice'],acceptcondition['choice'])
+# print("T-statistic value:", t_stat)
+# print('P-value:', p_value)
 
-#%%
-#paired samples t-test for condition by stress
-t_stat, p_value = ttest_rel(rejectioncondition['stress'],acceptcondition['stress'])
-print("T-statistic value:", t_stat)
-print('P-value:', p_value)
-#%%
+# #%% paired samples t-test for condition by salience
+# t_stat, p_value = ttest_rel(rejectioncondition['salience'],acceptcondition['salience'])
+# print("T-statistic value:", t_stat)
+# print('P-value:', p_value)
+
+# #%%
+# #paired samples t-test for condition by stress
+# t_stat, p_value = ttest_rel(rejectioncondition['stress'],acceptcondition['stress'])
+# print("T-statistic value:", t_stat)
+# print('P-value:', p_value)
+# #%%
