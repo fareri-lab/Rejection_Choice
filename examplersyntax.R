133c13223d65fd44f50499c72041b71639e32437
dfareri <dfareri@users.noreply.github.com>
2023-11-15 12:46:12 -0500

    Logistic Regression, DF

--- examplersyntax.R
+++ examplersyntax.R
@@ -1,18 +1,22 @@
 getwd()
 
-setwd("/Users/jordansiegel/Documents/GitHub/Rejection_Choice")
+setwd("/Users/dfareri/Dropbox/Dominic/Github/fareri-lab/Rejection_Choice/")
 
 data<-read.csv(file="longformdata_DF.csv",header=TRUE, sep=',')
 
 short_data<-read.csv(file="shortformdata_DF.csv",header=TRUE,sep=",")
 
-test<-lm(short_data$choice~short_data$condition_recode)
-summary(test)
-
-glm1<-glmer(playlottery~condition_recode + age + sex + timebetween + order + (1|PROLIFIC_ID),family=binomial,data=data)
-summary(glm1)
-
 data$condition_recode[data$condition_recode==2]<- -1
 unique(data$condition_recode)
 
-plot_model(glm1,type=c("pred"))
+data$playlottery[data$playlottery==999]<-NA
+test<-lm(short_data$choice~short_data$condition_recode)
+summary(test)
+
+glmer1<-glmer(playlottery~condition_recode + age + sex + timebetween + order + (1|PROLIFIC_ID),family=binomial,data=data)
+summary(glmer1)
+
+glm1<-glm(playlottery~condition_recode + age + sex + timebetween + order,family=binomial,data=data)
+
+plot_model(glm1,type=c("pred"),axis.title = c('Social Condition','Predicted Probability of Playing Lottery'),title='',colors=("circus"),mrdt.values="meansd")
+
